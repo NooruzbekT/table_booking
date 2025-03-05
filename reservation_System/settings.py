47c96b12052bcb701d714016e0a464eb9727106b
Toktobaev Nooruzbek <nokatoktobaev@gmail.com>
2025-03-05 20:26:23 +0500

    Initial commit

--- reservation_System/settings.py
+++ reservation_System/settings.py
@@ -148,7 +148,7 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "nookentoktobaev@gmail.com"
-EMAIL_HOST_PASSWORD = "mbke iwrz plnv ccpe"
+EMAIL_HOST_PASSWORD = "код кароче"
 
 from datetime import timedelta
 
